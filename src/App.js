4b0ab915473c48ec33eb66a629a2a12a9c2d738d
reubber <reubbers@alu.ufc.br>
2020-07-14 20:25:02 -0300

    implementacao inicial

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react'
 
-function App () {
-  return (
+const App = () => (
+  <div>
+    <input type='text' />
+    <ul>
+      <li>item 1</li>
+      <li>item 2</li>
+      <li>item 3</li>
+      <li>item 4</li>
+      <li>item 5</li>
+    </ul>
+
     <div>
-      <header>
-        <p>Hello worldas</p>
-      </header>
+      <h3>Mostrar</h3>
+      <a href=''>Todos</a> | <a href=''>Finalizados</a> | <a href=''>A fazer</a>
     </div>
-  )
-}
+  </div>
+)
 
 export default App
