d51078631064764420f7749344bad8a2cd349992
reubber <reubbers@alu.ufc.br>
2020-07-16 20:57:04 -0300

    add new tests

--- src/reducers/todos/todos.test.js
+++ src/reducers/todos/todos.test.js
@@ -1,6 +1,6 @@
 import { expect } from 'chai'
 import deepFreeze from 'deep-freeze'
-import todos from './index'
+import todos, { initialState } from './index'
 import { ADD_TODO, TOGGLE_TODO } from './actions'
 
 it('should todos be a function', () => {
@@ -57,6 +57,41 @@ it('should toggle first todo', () => {
     { id: 0, text: 'Hey', completed: true },
     { id: 1, text: 'Ho', completed: false }
   ]
+
+  expect(todos(before, action)).to.be.deep.equal(after)
+})
+
+it('should toggle second todo', () => {
+  const before = deepFreeze([
+    { id: 0, text: 'Hey', completed: false },
+    { id: 1, text: 'Ho', completed: false }
+  ])
+
+  const action = deepFreeze({
+    type: TOGGLE_TODO,
+    payload: { id: 1 }
+  })
+
+  const after = [
+    { id: 0, text: 'Hey', completed: false },
+    { id: 1, text: 'Ho', completed: true }
+  ]
+
+  expect(todos(before, action)).to.be.deep.equal(after)
+})
+
+it('should return the lasted state when action is unknown', () => {
+  const before = deepFreeze([{ id: 0, text: 'Hey', completed: false }])
+  const action = deepFreeze({ type: 'ANYTHING' })
+  const after = [{ id: 0, text: 'Hey', completed: false }]
+
+  expect(todos(before, action)).to.be.deep.equal(after)
+})
+
+it('should return the initialState when state before is unknown', () => {
+  const before = undefined
+  const action = deepFreeze({})
+  const after = initialState
 
   expect(todos(before, action)).to.be.deep.equal(after)
 })
