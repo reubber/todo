f48a39dbc183962845734c6518c469ad38b03715
reubber <reubbers@alu.ufc.br>
2020-07-20 22:02:15 -0300

    add filter todos

--- src/components/filter/index.js
+++ src/components/filter/index.js
@@ -1,10 +1,44 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import * as actions from '@reducers/visibility-filter/actions'
+import { setVisibilityFilter } from '@reducers/visibility-filter/action-creator'
+import FilterLink from './filter-link'
 
-const Filter = () => (
+const Filter = ({ activeFilter, handleFilter }) => (
+
   <div>
     <h3>Mostrar</h3>
-    <a href='##'>Todos</a> | <a href='##'>Finalizados</a> | <a href='###'>A fazer</a>
+    {filterItems.map((item) => (
+      <FilterLink
+        key={item.action}
+        action={item.action}
+        actionFilter={activeFilter}
+        onClick={handleFilter(item.action)}
+      >
+        {item.label}
+      </FilterLink>
+    ))}
   </div>
+
 )
 
-export default Filter
+const filterItems = [
+  { label: 'Todos', action: actions.SHOW_ALL },
+  { label: 'Finalizados', action: actions.SHOW_COMPLETED },
+  { label: 'A fazer', action: actions.SHOW_ACTIVE }
+]
+
+// ['Todos', 'Finalizador', 'A fazer']
+
+const mapStateToProps = (state) => ({
+  activeFilter: state.visibilityFilter
+})
+
+const mapDispatchToProps = (dispatch) => ({
+  handleFilter: (filter) => (e) => {
+    e.preventDefault()
+    dispatch(setVisibilityFilter(filter))
+  }
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Filter)
--- src/components/todos-list/index.js
+++ src/components/todos-list/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { toggleTodo } from '@reducers/todos/actions-creator'
+import * as filterActions from '@reducers/visibility-filter/actions'
 
-const TodoList = ({ todos, handleToggleTodo }) => (
+const TodoList = ({ todos, activeFilter, handleToggleTodo }) => (
   <ul>
-    {todos.map((todo) => (
+    {getVisibleTodos(todos, activeFilter).map((todo) => (
       <li
         key={todo.id}
         style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}
@@ -16,14 +17,23 @@ const TodoList = ({ todos, handleToggleTodo }) => (
   </ul>
 )
 
+const getVisibleTodos = (todos, activeFilter) => {
+  const filterItens = {
+    [filterActions.SHOW_ALL]: todos,
+    [filterActions.SHOW_COMPLETED]: todos.filter((todo) => todo.completed),
+    [filterActions.SHOW_ACTIVE]: todos.filter((todo) => !todo.completed)
+  }
+  return filterItens[activeFilter]
+}
+
 const mapStateToProps = (state) => ({
-  todos: state.todos
+  todos: state.todos,
+  activeFilter: state.visibilityFilter
 })
 
 const mapDispatchToProps = (dispatch) => ({
   handleToggleTodo: (id) => (e) => {
     dispatch(toggleTodo(id))
-    console.log('dd')
   }
 })
 
